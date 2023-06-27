759764a10f7a886d8f535a0a677ac292a7384d7c
diegokjgv <134401520+diegokjgv@users.noreply.github.com>
2023-06-27 10:48:11 -0400

    Add files via upload

---
+++ Diego_Espinoza_PGY21_015D.py
@@ -0,0 +1,18 @@
+opcion=0
+print("Elija una opcion")
+if opcion=1
+    print("ingrese nombre del jugador")
+    print("ingrese rut del jugador")
+    print("ingrese fecha nacimiento del jugador")
+    print("ingrese categoria del jugador")
+    print("ingrese celular del jugador")
+    print("ingrese nombre de la pareja")
+else
+    opcion=2
+
+else
+opcion=3
+
+else 
+    opcion=4
+    print("a salido del programa")
